67196ceb56b1e5a61a2332fb97e7df07e9b1add5
smntsng <144932270+smntsng@users.noreply.github.com>
2023-11-13 16:07:45 +0000

    added JS to hide start screen content on click

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,7 @@
-console.log("this is the logic jS")
+// click and hide starting quix content
+var startScreenEl = document.getElementById("start-screen");
+startScreenEl.addEventListener("click", hideStartScreen);
+
+function hideStartScreen() {
+    startScreenEl.style.display = 'none';
+}
